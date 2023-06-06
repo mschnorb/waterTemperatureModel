a560cf6c9781ff914e20fe943e955bab442b170f
Markus Schnorbus <mschnorb@cedar5.cedar.computecanada.ca>
2023-06-06 11:16:51 -0700

    Fix to also hold m2tDischarge constant.

--- model/routing.py
+++ model/routing.py
@@ -1764,7 +1764,7 @@ class Routing(object):
         #self.avgDischarge = self.avgDischarge + deltaAnoDischarge/\
         #                    pcr.min(self.maxTimestepsToAvgDischargeLong, self.timestepsToAvgDischarge)
         #self.avgDischarge = pcr.max(0.0, self.avgDischarge)
-        self.m2tDischarge = self.m2tDischarge + pcr.abs(deltaAnoDischarge*(dischargeUsed - self.avgDischarge))
+        #self.m2tDischarge = self.m2tDischarge + pcr.abs(deltaAnoDischarge*(dischargeUsed - self.avgDischarge))
         #
         # - short term average discharge
         #
